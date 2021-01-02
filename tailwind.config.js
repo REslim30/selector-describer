1c0389f46cdbeeedb85c20a0c0936b1b9033dd0f
Aidan Do <giahuydo99@gmail.com>
2021-01-02 16:09:17 +1030

    About page finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,10 +29,11 @@ module.exports = {
           '900': '#003E6B',
         }
       },
+      fontFamily: {
+        'sans': '"Asap", ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";'
+      }
     },
-    fontFamily: {
-      'mono': '"Anonymous Pro", ui-monospace, SFMono-Regular, Menlo, Monaco, Consolas, "Liberation Mono", "Courier New", monospace',
-    }
+
   },
   variants: {
     extend: {},
